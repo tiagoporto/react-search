2920ed7c99d8c0c9aa5f8f78f126470e6001a886
Tiago Porto <tiagoporto@ymail.com>
2018-10-21 14:57:21 -0300

    Updates

---
+++ precache-manifest.ddbfa81936d73568ec5139bb1c84019c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd1113bcb302a7c5b45119282a71f391",
+    "url": "/inkitt/static/media/loading.cd1113bc.svg"
+  },
+  {
+    "revision": "4a82ff824bc1c133470b",
+    "url": "/inkitt/static/js/runtime~main.4a82ff82.js"
+  },
+  {
+    "revision": "f848a2a75a3ad700a21e",
+    "url": "/inkitt/static/js/main.f848a2a7.chunk.js"
+  },
+  {
+    "revision": "9fc01f4ff34c47e18ecf",
+    "url": "/inkitt/static/js/1.9fc01f4f.chunk.js"
+  },
+  {
+    "revision": "f848a2a75a3ad700a21e",
+    "url": "/inkitt/static/css/main.42ef5976.chunk.css"
+  },
+  {
+    "revision": "9fc01f4ff34c47e18ecf",
+    "url": "/inkitt/static/css/1.16d36e6a.chunk.css"
+  },
+  {
+    "revision": "7a9949267f34d82f58c08ff755653aa2",
+    "url": "/inkitt/index.html"
+  }
+];
