3373488b23534cd6038f0ab9b1699be7e99a480d
Tiago Porto <tiago@tiagoporto.com>
2021-04-08 23:00:17 -0300

    Change project name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import Search from './components/search/Search.js';
-import './App.css';
+import React, { Component } from "react";
+import Search from "./components/search/Search.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Inkitt</h1>
-          <p className="App-intro">
-            <a href="https://github.com/tiagoporto/" className="App-link">Tiago Porto</a>'s test
-          </p>
+          <h1 className="App-title">React Search</h1>
+          <p className="App-intro">'s test</p>
         </header>
 
         <main className="App-main">
-          <Search/>
+          <Search />
         </main>
       </div>
     );
